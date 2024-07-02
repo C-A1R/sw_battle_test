1fea7b7889735ff6b0ddd5af5e5fb031f44da615
Алексей Корсаков <a.korsakov@eastwind.ru>
2024-07-02 13:17:49 +0500

    fix templates

--- src/Game/Battle.cpp
+++ src/Game/Battle.cpp
@@ -6,6 +6,98 @@
 namespace sw
 {
 
+Battle::Battle()
+    : _units{std::make_shared<UnitRegister>()}
+    , _eventLog{std::make_shared<EventLog>()}
+{
+}
+
+void Battle::addCommand(io::CreateMap &&cmd)
+{
+    _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
+    {
+        if (!cmd.width || !cmd.height)
+        {
+            return;
+        }
+        if (_map)
+        {
+            // map has been already created
+            return;
+        }
+        _map.reset(new Map(_units, cmd.width, cmd.height));
+        _eventLog->log(tick, io::MapCreated{cmd.width, cmd.height});
+    });
+}
+
+void Battle::addCommand(io::SpawnWarrior &&cmd)
+{
+    _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
+    {
+        if (!_map)
+        {
+            return;
+        }
+        if (_map->spawn(cmd.unitId, cmd.x, cmd.y))
+        {
+            _units->insert(std::make_shared<Warrior>(cmd.unitId, cmd.hp, cmd.strength));
+        }
+        _eventLog->log(tick, io::UnitSpawned{cmd.unitId, "Warrior", cmd.x, cmd.y });
+    });
+}
+
+void Battle::addCommand(io::SpawnArcher &&cmd)
+{
+    _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
+    {
+        if (!_map)
+        {
+            return;
+        }
+
+        std::shared_ptr<IUnit> unit = std::make_shared<Archer>(cmd.unitId, cmd.hp, cmd.strength, cmd.range, cmd.agility);
+        if (_map->spawn(cmd.unitId, cmd.x, cmd.y))
+        {
+            _units->insert(unit);
+        }
+        _eventLog->log(tick, io::UnitSpawned{cmd.unitId, "Archer", cmd.x, cmd.y });
+    });
+}
+
+void Battle::addCommand(io::March &&cmd)
+{
+    _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick) mutable
+    {
+        auto unit = _units->find(cmd.unitId);
+        if (!unit)
+        {
+            return;
+        }
+
+        std::vector<std::shared_ptr<IUnitAction>> actions;
+        splitMarch(std::move(cmd), actions);
+        for (int i = 0; i < actions.size(); ++i)
+        {
+            unit->addAction(actions[i]);
+        }
+    });
+}
+
+void Battle::addCommand(io::Wait &&cmd)
+{
+    _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick) mutable
+    {
+        for(auto i = _units->begin(); i != _units->end(); ++i)
+        {
+            std::shared_ptr<IUnitAction> act = std::make_shared<WaitAction>();
+            for (int j = 0; j < cmd.ticks; ++j)
+            {
+                i->second->addAction(std::move(act));
+            }
+        }
+    });
+}
+
 void Battle::run()
 {
     uint32_t tick = 1;
@@ -109,7 +201,6 @@ void Battle::splitMarch(io::March &&cmd, std::vector<std::shared_ptr<IUnitAction
             --tmpPoint.y;
         }
     };
-    auto unit = _units->find(cmd.unitId);
     while (!(tmpPoint.x == cmd.targetX && tmpPoint.y == cmd.targetY))
     {
         _doStep();
--- src/Game/Battle.hpp
+++ src/Game/Battle.hpp
@@ -35,102 +35,13 @@ class Battle
     std::queue<std::function<void(uint32_t &)>> _startup_actions; ///< actions for creating the game
 
 public:
-    Battle()
-        : _units{std::make_shared<UnitRegister>()}
-        , _eventLog{std::make_shared<EventLog>()}
-    {
-    }
+    Battle();
 
-    template<typename TCommand>
-    void addCommand(TCommand &&cmd)
-    {
-    }
-    template<>
-    void addCommand(io::CreateMap &&cmd)
-    {
-        _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
-        {
-            if (!cmd.width || !cmd.height)
-            {
-                return;
-            }
-            if (_map)
-            {
-                // map is already created
-                return;
-            }
-            _map.reset(new Map(_units, cmd.width, cmd.height));
-            _eventLog->log(tick, io::MapCreated{cmd.width, cmd.height});
-        });
-    }
-    template<>
-    void addCommand(io::SpawnWarrior &&cmd)
-    {
-        _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
-        {
-            if (!_map)
-            {
-                return;
-            }
-            std::shared_ptr<IUnit> unit = std::make_shared<Warrior>(cmd.unitId, cmd.hp, cmd.strength);
-            if (_map->spawn(cmd.unitId, cmd.x, cmd.y))
-            {
-                _units->insert(unit);
-            }
-            _eventLog->log(tick, io::UnitSpawned{cmd.unitId, "Warrior", cmd.x, cmd.y });
-        });
-    }
-    template<>
-    void addCommand(io::SpawnArcher &&cmd)
-    {
-        _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick)
-        {
-            if (!_map)
-            {
-                return;
-            }
-            std::shared_ptr<IUnit> unit = std::make_shared<Archer>(cmd.unitId, cmd.hp, cmd.strength, cmd.range, cmd.agility);
-            if (_map->spawn(cmd.unitId, cmd.x, cmd.y))
-            {
-                _units->insert(unit);
-            }
-            _eventLog->log(tick, io::UnitSpawned{cmd.unitId, "Archer", cmd.x, cmd.y });
-        });
-    }
-    template<>
-    void addCommand(io::March &&cmd)
-    {
-        _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick) mutable
-        {
-            auto unit = _units->find(cmd.unitId);
-            if (!unit)
-            {
-                return;
-            }
-
-            std::vector<std::shared_ptr<IUnitAction>> actions;
-            splitMarch(std::move(cmd), actions);
-            for (int i = 0; i < actions.size(); ++i)
-            {
-                unit->addAction(actions[i]);
-            }
-        });
-    }
-    template<>
-    void addCommand(io::Wait &&cmd)
-    {
-        _startup_actions.emplace([cmd = std::move(cmd), this](uint32_t &tick) mutable
-        {
-            for(auto i = _units->begin(); i != _units->end(); ++i)
-            {
-                std::shared_ptr<IUnitAction> act = std::make_shared<WaitAction>();
-                for (int j = 0; j < cmd.ticks; ++j)
-                {
-                    i->second->addAction(std::move(act));
-                }
-            }
-        });
-    }
+    void addCommand(io::CreateMap &&cmd);
+    void addCommand(io::SpawnWarrior &&cmd);
+    void addCommand(io::SpawnArcher &&cmd);
+    void addCommand(io::March &&cmd);
+    void addCommand(io::Wait &&cmd);
 
     void run();
 
